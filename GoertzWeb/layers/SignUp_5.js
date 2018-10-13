b663f371d4684710b2f415e5c41dfa70429abb39
cramos95 <livestrong095@yahoo.com>
2018-10-13 13:09:31 -0500

    Overhaul colors and achievements

--- GoertzWeb/layers/SignUp_5.js
+++
@@ -1,8 +0,0 @@
-var json_SignUp_5 = {
-"type": "FeatureCollection",
-"name": "SignUp_5",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "Label": "Sign-Up" }, "geometry": { "type": "Point", "coordinates": [ -97.434107197069196, 30.017624670440583 ] } }
-]
-}
--- GoertzWeb/layers/Start_3.js
+++
@@ -1,8 +0,0 @@
-var json_Start_3 = {
-"type": "FeatureCollection",
-"name": "Start_3",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "Label": "Start" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ -97.43405914221762, 30.017385733648162 ], [ -97.433560409188047, 30.017506326471434 ], [ -97.433296632143552, 30.016939339691497 ], [ -97.433687795942063, 30.016817085592528 ], [ -97.433743700112245, 30.016875705002285 ], [ -97.43405914221762, 30.017385733648162 ] ] ] } }
-]
-}
